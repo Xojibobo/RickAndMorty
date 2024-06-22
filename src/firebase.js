8bd7800401f082a9e61ce39504e46c6be47cdb13
Xojiakbar <xnabyev22@gmail.com>
2024-06-22 11:25:18 +0500

    last change

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDAew4yMKOSuSc2hYluxVxv-npi5XGtpGE",
+    authDomain: "rickandmorty-d80cb.firebaseapp.com",
+    projectId: "rickandmorty-d80cb",
+    storageBucket: "rickandmorty-d80cb.appspot.com",
+    messagingSenderId: "740912751535",
+    appId: "1:740912751535:web:edd61c0eac6c1dbdc6055a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
